2eb27958b89bcd76c25f9f33f7a358a2de5934bf
EternallyAscend <EternallyAscend>
2026-04-15 14:30:34 +0800

    Fix privacy notification

---
+++ app/src/main/java/com/tom/nono/service/ListenerKeepAliveReceiver.kt
@@ -0,0 +1,21 @@
+package com.tom.nono.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class ListenerKeepAliveReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action ?: return
+        if (action == Intent.ACTION_BOOT_COMPLETED || action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            val serviceIntent = Intent(context, ListenerKeepAliveService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
+
